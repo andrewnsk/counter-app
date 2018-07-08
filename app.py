210b2b3a701ddd4056f70e3355111dfe2954c036
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-07-08 12:16:05 +0700

    add redis_hostname environ variable

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 import time
 import redis
 from flask import Flask
 
+redis_hostname = os.environ.get('redis_hostname', 'redis')
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host=redis_hostname, port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -16,10 +19,12 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+
 @app.route('/')
 def hello():
     count = get_hit_count()
     return "Big Ass hole counter: {} queries\n".format(count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
