f4affc920663f06db71c1b6a58a83ca33386f467
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-07-08 12:23:11 +0700

    add 404 counter

--- app.py
+++ app.py
@@ -20,10 +20,28 @@ def get_hit_count():
             time.sleep(0.5)
 
 
+def get_error_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('error_hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
 @app.route('/')
 def hello():
     count = get_hit_count()
     return "Big Ass hole counter: {} queries\n redis_hostname\n".format(count)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    error_count = get_error_hit_count()
+    return "error count: {0}".format(error_count)
 
 
 if __name__ == "__main__":
